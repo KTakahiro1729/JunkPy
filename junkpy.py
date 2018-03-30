a242658dddce6343664ab2a085a20e9bc7871a2c
KTakahiro1729 <poet.takahiro1729@gmail.com>
2018-03-31 06:09:42 +0900

    split walk_child to parse_child and deploy_child and actually use JunkType.child_nodetype

--- junkpy.py
+++ junkpy.py
@@ -2,18 +2,9 @@
 import ast
 import copy
 from collections import namedtuple
+import builtins
 
 Node = namedtuple("Node", "nodetype number")
-
-#terminal
-class identifier():
-    pass
-class string():
-    pass
-class singleton():
-    pass
-class constant():
-    pass
 
 class JunkStruct():
     def __init__(self, head = "", neck = "", shoulder = "", body = "", foot = ""):
@@ -34,7 +25,7 @@ class JunkType():
     def node_type(self):
         return ast.AST
     @property
-    def childnode_type(self):
+    def child_nodetype(self):
         return dict([])
     def check_node_type(self):
         if type(self.node) != self.node_type:
@@ -50,18 +41,32 @@ class JunkType():
         self.connector = connector
         self.indent = indent
         self.childnodes = self.parse_childnode()
-        self.walk_child()
+        self.deploy_child()
         self.output = self.struct.join(connector)
-    def walk_child(self):
+    def deploy_child(self):
         pass
     def make_junk(self, child, lower_node = None):
+        ast_type = type(child)
+        builtin_types = [getattr(builtins, attr) for attr in dir(builtins)]
+        if ast_type in builtin_types:
+            return JunkTerminal()
         for junktype in lower_node:
-            if type(child) == junktype.node_type:
+            if ast_type == junktype.node_type:
                 return junktype(node = child, connector = self.connector)
         else:
-            raise Exception("{0} not in candidate : {1}".format(type(child),candidate))
+            raise Exception("{0} not in candidate : {1}".format(type(child),lower_node))
     def parse_childnode(self):
-        pass
+        result = dict([])
+        for child_name in self.node._fields:
+            field = getattr(self.node, child_name)
+            node = self.child_nodetype[child_name]
+            if  node.number in ["", "?"]:
+                result[child_name] = self.make_junk(field, lower_node = node.nodetype)
+            elif node.number == "*":
+                result[child_name] = \
+                    [self.make_junk(one_node, lower_node = node.nodetype).struct
+                        for one_node in field]
+        return result
     def make_block(self, exprs):
 
         block_struct = JunkStruct(
@@ -71,16 +76,17 @@ class JunkType():
         for child in ast.iter_child_nodes(self.node):
             block_struct += self.make_junk(child, self.connector).struct
         return block_struct
+
 #mod
 class JunkModule(JunkType):
     node_type = ast.Module
     @property
-    def childnode_type(self):
+    def child_nodetype(self):
         return {"body": Node(stmt, "*")}
     def __init__(self, *args,save_ns = False, **kwargs):
         self.save_ns = save_ns
         super().__init__(*args, **kwargs)
-    def walk_child(self):
+    def deploy_child(self):
         self.struct.head     += "[ns " if self.save_ns else "[None "
         ns_init = "".join([
             "__builtins__ ",
@@ -95,13 +101,17 @@ class JunkModule(JunkType):
                 lower_node = stmt
             ).struct
 
+#terminal
+class JunkTerminal():
+    pass
+
 #stmt
 class JunkExpr(JunkType):
     node_type = ast.Expr
     @property
-    def childnode_type(self):
+    def child_nodetype(self):
         return {"value": Node(expr, "")}
-    def walk_child(self):
+    def deploy_child(self):
         value = self.make_junk(self.node.value, expr)
         self.struct +=  value.struct
         self.struct.neck += "for ns in[ns "
@@ -110,9 +120,9 @@ class JunkExpr(JunkType):
 class JunkAssign(JunkType):
     node_type = ast.Assign
     @property
-    def childnode_type(self):
+    def child_nodetype(self):
         return {"targets": Node(expr, "*"), "value": Node(expr, "")}
-    def walk_child(self):
+    def deploy_child(self):
         targets = [self.make_junk(target,expr) for target in self.node.targets]
         value = self.make_junk(self.node.value, expr)
         self.struct.neck = "for ns in[ns "
@@ -126,11 +136,11 @@ class JunkAssign(JunkType):
 class JunkIf(JunkType):
     node_type = ast.If
     @property
-    def childnode_type(self):
+    def child_nodetype(self):
         return {"test": Node(expr, ""),
                    "args": Node(operator, "*"),
                    "orelse": Node(expr, "")}
-    def walk_child(self):
+    def deploy_child(self):
         test = self.make_junk(self.
             node.test,
             lower_node = expr)
@@ -146,11 +156,11 @@ class JunkIf(JunkType):
 class JunkBinOp(JunkType):
     node_type = ast.BinOp
     @property
-    def childnode_type(self):
+    def child_nodetype(self):
         return {"left": Node(expr, ""),
                    "op": Node(operator, ""),
                    "right": Node(expr, "")}
-    def walk_child(self):
+    def deploy_child(self):
         left = self.make_junk(self.node.left, expr)
         op = self.make_junk(self.node.op, operator)
         right =self.make_junk(self.node.right, expr)
@@ -162,10 +172,10 @@ class JunkBinOp(JunkType):
 class JunkDict(JunkType):
     node_type = ast.Dict
     @property
-    def childnode_type(self):
-        return {"keyes": Node(expr, "*"),
+    def child_nodetype(self):
+        return {"keys": Node(expr, "*"),
                    "values": Node(expr, "*")}
-    def walk_child(self):
+    def deploy_child(self):
         keys = [self.make_junk(k, expr) for k in self.node.keys]
         values = [self.make_junk(v, expr) for v in self.node.values]
         zipped = zip(keys, values)
@@ -174,11 +184,11 @@ class JunkDict(JunkType):
 class JunkCall(JunkType):
     node_type = ast.Call
     @property
-    def childnode_type(self):
+    def child_nodetype(self):
         return {"func": Node(expr, ""),
                    "args": Node(expr, "*"),
-                   "keyword": Node(expr, "*")}
-    def walk_child(self):
+                   "keywords": Node(expr, "*")}
+    def deploy_child(self):
         func = self.make_junk(self.node.func, expr)
         args = [self.make_junk(arg, expr) for arg in self.node.args]
         args_str = ",".join([arg.struct.body for arg in args])
@@ -190,53 +200,52 @@ class JunkCall(JunkType):
 class JunkNum(JunkType):
     node_type = ast.Num
     @property
-    def childnode_type(self):
-        return {"n": Node(object, "")}
-    def walk_child(self):
+    def child_nodetype(self):
+        return {"n": Node(JunkTerminal, "")}
+    def deploy_child(self):
         self.struct.body += str(self.node.n)
 
 class JunkStr(JunkType):
     node_type = ast.Str
     @property
-    def childnode_type(self):
-        return {"s": Node(object, "")}
-    def walk_child(self):
+    def child_nodetype(self):
+        return {"s": Node(JunkTerminal, "")}
+    def deploy_child(self):
         self.struct.body += "\"" + self.node.s + "\""
 
 class JunkNameConstant(JunkType):
     node_type = ast.NameConstant
     @property
-    def childnode_type(self):
-        return {"value": Node(singleton, "")}
-    def walk_child(self):
+    def child_nodetype(self):
+        return {"value": Node(JunkTerminal, "")}
+    def deploy_child(self):
         self.struct.body = str(self.node.value)
 
 
 class JunkName(JunkType):
     node_type = ast.Name
     @property
-    def childnode_type(self):
-        return {"id": Node(idnetifier, ""), "ctx":Node(expr_context, "")}
-    def walk_child(self):
+    def child_nodetype(self):
+        return {"id": Node(JunkTerminal, ""), "ctx":Node(expr_context, "")}
+    def deploy_child(self):
         self.struct.body = "ns[\"{0}\"]".format(self.node.id)
 
 #expr_context
 class JunkLoad(JunkType):
     node_type = ast.Load
-    def walk_child(self):
+    def deploy_child(self):
         pass
 
 class JunkStore(JunkType):
     node_type = ast.Store
-    def walk_child(self):
+    def deploy_child(self):
         pass
 
 class JunkAdd(JunkType):
     node_type = ast.Add
-    def walk_child(self):
+    def deploy_child(self):
         self.struct.body = "+"
 
-terminal = [identifier, int, string, bytes, object, singleton, constant]
 mod = [JunkModule]
 stmt = [JunkExpr, JunkAssign, JunkIf]
 expr = [JunkBinOp, JunkDict, JunkCall, JunkNum, JunkStr, JunkNameConstant, JunkName]
